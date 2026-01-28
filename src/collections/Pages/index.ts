e1b71d1581234ce28de7b9d98d65cf8583ca2eb2
janhodic <jan.hodic@datapac.sk>
2026-01-28 14:20:51 +0100

    pages

---
+++ src/collections/Pages/index.ts
@@ -0,0 +1,12 @@
+// src/collections/Posts.ts
+import type { CollectionConfig } from 'payload'
+
+export const Page: CollectionConfig = {
+  slug: 'post',
+  admin: { useAsTitle: 'title' },
+  fields: [
+    { name: 'title', type: 'text', required: true },
+    { name: 'slug', type: 'text', required: true, unique: true },
+    { name: 'content', type: 'textarea' },
+  ],
+}
