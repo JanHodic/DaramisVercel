06e46ac09ff6a250a064945c4daa9e6d42b9c305
janhodic <jan.hodic@datapac.sk>
2026-01-28 16:14:59 +0100

    migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260128_144959 from './20260128_144959';
+import * as migration_20260128_151441 from './20260128_151441';
 
 export const migrations = [
   {
     up: migration_20260128_144959.up,
     down: migration_20260128_144959.down,
-    name: '20260128_144959'
+    name: '20260128_144959',
+  },
+  {
+    up: migration_20260128_151441.up,
+    down: migration_20260128_151441.down,
+    name: '20260128_151441'
   },
 ];
